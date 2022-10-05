f770cce3c4f50df43e866f32c7cb86cff25a29a8
kcw <dwong1375@gmail.com>
2022-10-04 18:56:21 -0700

    split jcamp response into lines

--- spectrum.py
+++ spectrum.py
@@ -8,4 +8,8 @@ def get_jcampdx(molecule_id, index):
     index: an int, representing the spectrum index for the molecule
     '''
     # get the IR spectrum JCAMP-DX data
-    return requests.get(f'https://webbook.nist.gov/cgi/cbook.cgi?JCAMP={molecule_id}&amp;Index={index}&amp;Type=IR').text
+    api_url=f'https://webbook.nist.gov/cgi/cbook.cgi?JCAMP={molecule_id}&amp;Index={index}&amp;Type=IR'
+    response = requests.get(api_url)
+    content = response.content.splitlines()
+    content = [line.decode("utf-8") for line in content]
+    return content
