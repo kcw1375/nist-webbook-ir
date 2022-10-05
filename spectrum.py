5e15f32bdb59c01a31ba12b2499db9b8a62d0637
kcw <dwong1375@gmail.com>
2022-10-04 20:25:04 -0700

    add search function to get molecule ids

--- spectrum.py
+++ spectrum.py
@@ -2,6 +2,8 @@
 
 import requests
 from jcamp import jcamp_read
+from bs4 import BeautifulSoup
+import re
 
 def get_jcampdx(molecule_id, index):
     '''
@@ -17,3 +19,32 @@ def get_jcampdx(molecule_id, index):
 
 def parse_jcampdx(content):
     return jcamp_read(content)
+
+def search(term):
+    '''
+    use the NIST webbook search feature to get molecule IDs
+
+    Parameters:
+    term: a string, the search term
+
+    Returns:
+    a dictionary keyed by molecule ID, with values of name and chemical formula 
+    '''
+
+    search_url = f'https://webbook.nist.gov/cgi/cbook.cgi?Name={term}&Units=SI&cIR=on'
+    response = requests.get(search_url)
+
+    # parse html to get search results
+    soup = BeautifulSoup(response.content)
+    results = dict()
+
+    for result in soup.find('ol').find_all('li'):
+        atag = result.find('a')
+        # get id from the parameter in the href link
+        # this regex extracts only the molecule id
+        molecule_id = re.search('ID=.+?\&', atag['href']).group()[3:-1]
+        molecule_name = atag.contents[0]
+        formula = result.contents[1:] #todo: simplify formula
+        results[molecule_id] = (molecule_name, formula)
+
+    return results
